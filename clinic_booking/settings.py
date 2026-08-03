46624567a717d3b3e4f77d12f5dcd483d952d16a
Anthony Nyanumba <nyanumbaanthony@gmail.com>
2026-08-03 22:30:53 +0300

    added whitenoise static for render pages

--- clinic_booking/settings.py
+++ clinic_booking/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'clinic_booking.urls'
@@ -136,6 +137,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STORAGES = {
+  "staticfiles": {"BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+},
+}
 
 
 REST_FRAMEWORK = {
